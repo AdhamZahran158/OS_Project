0bc92e70c23f81f338fc10a7c209c9495463e296
Marwan-m-m <2200736@eng.asu.edu.eg>
2026-04-14 07:59:29 +0200

    update process

--- OS-Project/src/main/java/com/mycompany/os/project/src/process.java
+++ OS-Project/src/main/java/com/mycompany/os/project/src/process.java
@@ -4,10 +4,20 @@ public class process {
     public String processName;
     public int burst;
     public int priority;
-    public process(int burst, int priority, String Name)
+
+    public int arrivaltime;
+    public int finishtime;
+
+
+    public boolean finished= false;
+
+    public process(int burst, int priority, String Name )
     {
         this.burst = burst;
         this.priority = priority;
         this.processName = Name;
+
     }
+    public double getTAT (){return (double)finishtime-arrivaltime;}
+    public double getWT (){return (double)finishtime-arrivaltime-burst;}
 }
